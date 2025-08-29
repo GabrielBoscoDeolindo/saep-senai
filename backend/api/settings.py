4fa89116a5fb8bf00f71d4d1d48d71c3b90c9835
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-08-29 16:09:20 -0300

    feat: Add cors to backend

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'app',
     "rest_framework",
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  
 ]
 
 ROOT_URLCONF = 'api.urls'
